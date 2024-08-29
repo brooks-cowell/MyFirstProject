18b35a2baca29943c904704ffd03700f5e393d76
brooks-cowell <brooks11.cowell@gmail.com>
2024-08-29 10:38:36 -0500

    Add files via upload

---
+++ Main2.cpp
@@ -0,0 +1,17 @@
+/*
+Brooks Cowell
+Computer science Fall 2024
+Due: Sept. 3 2024
+Lab 1, Lab 1 Introduction to C++ Programming
+This lab is a basic demo of the C++ language
+*/
+
+#include<iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	std::cout << "Red" << std::endl;
+	
+	return 0;
+}
